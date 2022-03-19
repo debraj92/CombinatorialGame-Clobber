d025a32f8cdbbf30476f2f2eaad154af16b57034
Debraj Ray <debrajray@Debrajs-MacBook-Pro.local>
2022-03-19 16:36:57 -0600

    Changes in CNN

--- cnn_control.py
+++ cnn_control.py
@@ -8,14 +8,25 @@ from search_basics import INFINITY, PROVEN_WIN, PROVEN_LOSS, UNKNOWN
 import numpy as np
 from keras.layers import Conv1D, Dense, MaxPooling1D, Flatten, BatchNormalization, ReLU, MaxPool1D, Dense, Dropout, \
     AveragePooling1D, GlobalAveragePooling1D
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 import keras.utils
 from matplotlib import pyplot as plt
 import tensorflow as tf
+import shutil
+import random
+from numpy import random
 
+'''
+Reuse checkpoints:
+checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+latest = tf.train.latest_checkpoint(checkpoint_dir)
+
+# Load the previously saved weights
+model.load_weights(latest)
+'''
 
 class cnn:
-
     elements = [".", "B", "W"]
 
     games_for_training = set()
@@ -26,7 +37,7 @@ class cnn:
 
     label = None
 
-    EPOCHS = 60
+    EPOCHS = 50
 
     sample_size = 0  # number of samples in train set
     time_steps = 0  # number of features in train set
@@ -34,6 +45,14 @@ class cnn:
     # [batch_size, time_steps, input_dimension] ---> [sample_size, time_steps, input_dimension]
 
     train_data_reshaped = None
+
+    def createRandomBoard(self, size):
+        elements = ["B", "W", "."]
+        board = ""
+        for i in range(size):
+            board += elements[np.random.randint(low=0, high=3)]
+
+        return board
 
     def generateGameCombinations(self, boardString, length, maxlength):
 
@@ -44,6 +63,7 @@ class cnn:
         for i in self.elements:
             self.generateGameCombinations(boardString + i, length + 1, maxlength)
 
+    '''
     def convertGameStringToInputVector(self, gameBoard):
         x = np.empty(shape=[0, 2])
         for i in range(self.MAX_LENGTH):
@@ -57,6 +77,36 @@ class cnn:
             else:
                 x = np.append(x, [[0, 0]], axis=0)
 
+        return x
+    '''
+
+    def convertGameBoardToVector(self, gameBoard):
+        x = np.empty(shape=[0, 2])
+        for i in range(len(gameBoard)):
+            if gameBoard[i] == "B":
+                x = np.append(x, [[0, 1]], axis=0)
+            elif gameBoard[i] == "W":
+                x = np.append(x, [[1, 0]], axis=0)
+            else:
+                x = np.append(x, [[0, 0]], axis=0)
+
+        return x
+
+    def createInputFeatureVector(self, gameBoard):
+        x = self.convertGameBoardToVector(gameBoard)
+        padding = random.randint(15)
+        dots_prefix = np.full((padding, 2), [[0, 0]], dtype=np.float32)
+        x = np.concatenate((dots_prefix, x), axis=0)
+        empty_positions_to_add = self.MAX_LENGTH - len(x)
+        dots_suffix = np.full((empty_positions_to_add, 2), [[0, 0]], dtype=np.float32)
+        x = np.concatenate((x, dots_suffix), axis=0)
+        '''
+        rand_num = np.random.random(1)[0]
+        if rand_num > 0.5:
+            x = np.concatenate((x, dots), axis=0)
+        else:
+            x = np.concatenate((dots, x), axis=0)
+        '''
         return x
 
     def createTrainingData(self, first_player=BLACK):
@@ -74,10 +124,10 @@ class cnn:
             print("Done")
             if outcome == PROVEN_WIN:
                 label.append(1)
-                train_data.append(self.convertGameStringToInputVector(game))
+                train_data.append(self.createInputFeatureVector(game))
             else:
                 label.append(0)
-                train_data.append(self.convertGameStringToInputVector(game))
+                train_data.append(self.createInputFeatureVector(game))
 
             count += 1
             complete = int(count / total * 100)
@@ -110,102 +160,273 @@ class cnn:
     def reshapeInput(self):
         self.sample_size = self.train_data.shape[0]
         self.time_steps = self.train_data.shape[1]
+        print("Sample size", self.sample_size)
+        print("time steps", self.time_steps)
         self.train_data = np.reshape(self.train_data, (self.sample_size, self.time_steps, self.input_dimension))
 
     def cnnModel(self):
         model = Sequential()
-        model.add(Conv1D(filters=32, kernel_size=7, strides=1, activation='relu',
+        model.add(Conv1D(filters=64, kernel_size=9, strides=1, activation='relu',
                          input_shape=(self.time_steps, self.input_dimension)))  # 1
-        model.add(Dropout(0.25))
-        model.add(MaxPool1D(pool_size=2, strides=2))
-        model.add(Conv1D(filters=16, kernel_size=5, activation='relu', strides=1))  # 2
-        model.add(MaxPool1D(pool_size=2, strides=2))
-        model.add(Conv1D(filters=8, kernel_size=3, activation='relu', strides=1))  # 3
-        model.add(MaxPool1D(pool_size=2, strides=2))
+        model.add(Dropout(0.2))
+        model.add(MaxPool1D(pool_size=2, strides=1))
+        model.add(Conv1D(filters=32, kernel_size=7, strides=1, activation='relu',
+                         input_shape=(self.time_steps, self.input_dimension)))  # 2
+        model.add(Dropout(0.1))
+        model.add(MaxPool1D(pool_size=2, strides=1))
+        model.add(Conv1D(filters=16, kernel_size=5, activation='relu', strides=1))  # 3
+        model.add(MaxPool1D(pool_size=2, strides=1))
+        model.add(Conv1D(filters=8, kernel_size=3, activation='relu', strides=1))  # 4
+        model.add(MaxPool1D(pool_size=2, strides=1))
         model.add(Flatten())
         model.add(Dense(64, activation='relu', name="Dense_1"))
         model.add(Dense(self.input_dimension, activation='softmax', name="Dense_2"))
         model.compile('adam', loss='mse', metrics=['accuracy'])
         return model
 
+    def reloadModel(self, type_player=BLACK):
 
-model = cnn()
-model.generateGameCombinations("", 0, 12)
-print("Games of size 11 generated")
-model.createTrainingData()
+        if type_player == BLACK:
+            return load_model('./clobber-black-cnn.h5')
 
-print(model.train_data.shape)  # number of games X number of features
-print(model.label.shape)  # number of games
-model.reshapeInput()
+        return load_model('./clobber-white-cnn.h5')
 
-model.serialize_train_data('train_data_samples-black.npy', 'labels-black.npy')
-# model.read_train_data('train_data_samples-black.npy', 'labels-black.npy')
 
-model_conv1D = model.cnnModel()
+class cnn_trainer:
 
-history = model_conv1D.fit(model.train_data, model.label, epochs=model.EPOCHS,
-                           validation_split=0.2, verbose=1)
+    RANDOM_COMBINATION = 1
+    ALL_COMBINATION = 2
 
-model_conv1D.save("clobber-black-cnn.h5")
+    def createModelFromScratch(self, board_size, combinations=ALL_COMBINATION, number_of_samples=None, skip_b_train=False):
+        model = cnn()
+        if combinations == self.ALL_COMBINATION:
+            model.generateGameCombinations("", 0, board_size)
+            print("Games of size 11 generated")
+        else:
+            for i in range(number_of_samples):
+                board = model.createRandomBoard(board_size)
+                model.games_for_training.add(board)
 
-plt.figure(1)
-plt.subplot(211)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
+            print("All samples generated")
+        if not skip_b_train:
+            model.createTrainingData()
 
-plt.subplot(212)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper right')
-plt.tight_layout()
-plt.show()
+            print(model.train_data.shape)  # number of games X number of features
+            print(model.label.shape)  # number of games
+            model.reshapeInput()
 
-print("Training Complete for black")
-# REPEAT SAME CODE FOR WHITE
+            #model.serialize_train_data('train_data_samples-black.npy', 'labels-black.npy')
+            # model.read_train_data('train_data_samples-black.npy', 'labels-black.npy')
 
-model_temp = model
+            model_conv1D = model.cnnModel()
 
-model = cnn()
-model.games_for_training = model_temp.games_for_training
+            # checkpoint
+            checkpoint_filepath = './checkpoints/b'
+            model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+                filepath=checkpoint_filepath,
+                save_weights_only=True,
+                monitor='val_accuracy',
+                mode='max',
+                save_best_only=True)
+                #save_freq=5 * model.sample_size)
 
-model.createTrainingData(WHITE)
-print(model.train_data.shape)  # number of games X number of features
-print(model.label.shape)  # number of games
-model.reshapeInput()
+            history = model_conv1D.fit(model.train_data, model.label, callbacks=[model_checkpoint_callback], epochs=model.EPOCHS,
+                                       validation_split=0.2, verbose=1)
 
-model.serialize_train_data('train_data_samples-white.npy', 'labels-white.npy')
-# model.read_train_data('train_data_samples-black.npy', 'labels-black.npy')
+            model_conv1D.save("clobber-black-cnn.h5")
 
-model_conv1D = model.cnnModel()
+            plt.figure(1)
+            plt.subplot(211)
+            plt.plot(history.history['accuracy'])
+            plt.plot(history.history['val_accuracy'])
+            plt.title('model accuracy')
+            plt.ylabel('accuracy')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'val'], loc='upper left')
 
-history = model_conv1D.fit(model.train_data, model.label, epochs=model.EPOCHS,
-                           validation_split=0.2, verbose=1)
-# plot_history(history)
-model_conv1D.save("clobber-white-cnn.h5")
+            plt.subplot(212)
+            plt.plot(history.history['loss'])
+            plt.plot(history.history['val_loss'])
+            plt.title('Model Loss')
+            plt.ylabel('Loss')
+            plt.xlabel('epoch')
+            plt.legend(['train', 'val'], loc='upper right')
+            plt.tight_layout()
+            #plt.show()
+            plt.savefig('performance-black.png')
 
-plt.figure(1)
-plt.subplot(211)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper left')
+            plt.clf()
 
-plt.subplot(212)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('Model Loss')
-plt.ylabel('Loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'val'], loc='upper right')
-plt.tight_layout()
-plt.show()
-print("Training Complete for white")
+            print("Training Complete for black")
+
+        # REPEAT SAME CODE FOR WHITE
+
+        model_temp = model
+
+        model = cnn()
+        model.games_for_training = model_temp.games_for_training
+
+        model.createTrainingData(WHITE)
+        print(model.train_data.shape)  # number of games X number of features
+        print(model.label.shape)  # number of games
+        model.reshapeInput()
+
+        #model.serialize_train_data('train_data_samples-white.npy', 'labels-white.npy')
+        # model.read_train_data('train_data_samples-black.npy', 'labels-black.npy')
+
+        model_conv1D = model.cnnModel()
+
+        # checkpoint
+        checkpoint_filepath = './checkpoints/w'
+        model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_filepath,
+            save_weights_only=True,
+            monitor='val_accuracy',
+            mode='max',
+            save_best_only=True)
+
+        history = model_conv1D.fit(model.train_data, model.label, callbacks=[model_checkpoint_callback], epochs=model.EPOCHS,
+                                   validation_split=0.2, verbose=1)
+        # plot_history(history)
+        model_conv1D.save("clobber-white-cnn.h5")
+
+        plt.figure(1)
+        plt.subplot(211)
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+
+        plt.subplot(212)
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('Model Loss')
+        plt.ylabel('Loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper right')
+        plt.tight_layout()
+        #plt.show()
+        plt.savefig('performance-white.png')
+        print("Training Complete for white")
+
+    def retrainModelWithSample(self, number_of_samples, board_size):
+        model = cnn()
+        for i in range(number_of_samples):
+            board = model.createRandomBoard(board_size)
+            model.games_for_training.add(board)
+
+        # black
+        model.createTrainingData()
+        model.reshapeInput()
+        model_conv1D = model.reloadModel()
+
+        # checkpoint
+        checkpoint_filepath = './checkpoints/b/b'
+        model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_filepath,
+            save_weights_only=True,
+            monitor='val_accuracy',
+            mode='max',
+            save_best_only=True)
+
+        history = model_conv1D.fit(model.train_data, model.label, callbacks=[model_checkpoint_callback], epochs=model.EPOCHS,
+                                   validation_split=0.2, verbose=1)
+
+        model_conv1D.save("clobber-black-cnn-retrained.h5")
+
+        plt.figure(1)
+        plt.subplot(211)
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+
+        plt.subplot(212)
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('Model Loss')
+        plt.ylabel('Loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper right')
+        plt.tight_layout()
+        plt.savefig('performance-black-retrained.png')
+        plt.clf()
+        print("Training Complete for black")
+
+        # white
+        model_temp = model
+
+        model = cnn()
+        model.games_for_training = model_temp.games_for_training
+
+        model.createTrainingData(WHITE)
+        model.reshapeInput()
+        model_conv1D = model.reloadModel(WHITE)
+        # checkpoint
+        checkpoint_filepath = './checkpoints/w/w'
+        model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_filepath,
+            save_weights_only=True,
+            monitor='val_accuracy',
+            mode='max',
+            save_best_only=True)
+
+        history = model_conv1D.fit(model.train_data, model.label, callbacks=[model_checkpoint_callback],  epochs=model.EPOCHS,
+                                   validation_split=0.2, verbose=1)
+
+        model_conv1D.save("clobber-white-cnn-retrained.h5")
+
+        plt.figure(1)
+        plt.subplot(211)
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('model accuracy')
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+
+        plt.subplot(212)
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('Model Loss')
+        plt.ylabel('Loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper right')
+        plt.tight_layout()
+        plt.savefig('performance-white-retrained.png')
+        print("Training Complete for white")
+
+    def finalizeRetraining(self):
+        shutil.copy("clobber-white-cnn-retrained.h5", "'clobber-white-cnn.h5'")
+        shutil.copy("clobber-black-cnn-retrained.h5", "'clobber-black-cnn.h5'")
+
+    def generateTFLite(self):
+        model = cnn()
+        model_conv1D = model.reloadModel()
+        converter = tf.lite.TFLiteConverter.from_keras_model(model_conv1D)
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        quantized_and_pruned_tflite_model = converter.convert()
+        with open('./clobber-black-cnn.tflite', 'wb') as f:
+            f.write(quantized_and_pruned_tflite_model)
+
+        model_conv1D = model.reloadModel(WHITE)
+        converter = tf.lite.TFLiteConverter.from_keras_model(model_conv1D)
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        quantized_and_pruned_tflite_model = converter.convert()
+        with open('./clobber-white-cnn.tflite', 'wb') as f:
+            f.write(quantized_and_pruned_tflite_model)
+
+
+t = cnn_trainer()
+
+#t.createModelFromScratch(18, t.RANDOM_COMBINATION, 400000, True)
+
+t.retrainModelWithSample(500000, 26)
+
+t.finalizeRetraining()
+
+t.generateTFLite()
--- inference_tflite.py
+++ inference_tflite.py
@@ -2,27 +2,211 @@ import time
 
 import tensorflow as tf
 import numpy as np
+from random import choices
 
+from boolean_negamax_tt import PlayClobber
 from clobber_1d import Clobber_1d
 
-clobber = Clobber_1d("WBWBWBWWWBWBW", 1, 1)  # Exp : White loses(0) [1 0]
-clobber.computePrunedMovesFromSubgames(0)
-X = clobber.board_features
-X = np.reshape(X, (1, 40, 2))
+games_for_training = []
 
-start = time.time()
-# Load TFLite model and allocate tensors.
-interpreter = tf.lite.Interpreter(model_path="clobber-black-cnn.tflite")
-interpreter.allocate_tensors()
-# Get input and output tensors.
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
+PROVEN_WIN = 10000
+PROVEN_LOSS = -10000
 
-interpreter.set_tensor(input_details[0]['index'], X)
-interpreter.invoke()
-output_data = interpreter.get_tensor(output_details[0]['index'])
-end = time.time()
-d = end - start
-print("time taken ", d)
-print(output_data)
+TEST_BLACK = True
+TEST_WHITE = True
 
+
+def createRandomBoard(size, skipDot):
+    if skipDot:
+        elements = ["B", "W"]
+    else:
+        elements = ["B", "W", "."]
+    board = ""
+    population = [0, 1, 2]
+    weights = [0.25, 0.25, 0.5]
+    for i in range(size):
+        index = choices(population, weights)
+        #board += elements[np.random.randint(low=0, high=3)]
+
+        board += elements[index[0]]
+
+    return board
+
+
+def generateGameCombinations(sample_size, boardSize, skipDots=False):
+    global games_for_training
+    for i in range(sample_size):
+        games_for_training.append(createRandomBoard(boardSize, skipDots))
+
+
+def repeatGame(game, count):
+    global games_for_training
+    for i in range(count):
+        games_for_training.append(game)
+
+
+def printGames():
+    global games_for_training
+    for i in range(len(games_for_training)):
+        print(games_for_training[i])
+
+
+def dotifyGame(game, boardSize, count_of_positons_to_delete):
+    dots = np.random.choice(boardSize - 1, count_of_positons_to_delete, replace=False)
+    for d in dots:
+        game = game[:d] + "." + game[d + 1:]
+
+    return game
+
+
+def dotifyGames(boardSize, count_of_positons_to_delete):
+    global games_for_training
+    for i in range(len(games_for_training)):
+        game = games_for_training[i]
+        games_for_training[i] = dotifyGame(game, boardSize, count_of_positons_to_delete)
+
+
+def compress():
+    global games_for_training
+    compressed_games = []
+    for i in range(len(games_for_training)):
+        newGame = ""
+        game = games_for_training[i]
+        previous_char = None
+        for p in game:
+            if previous_char is not None and previous_char == "." and p == ".":
+                continue
+            else:
+                newGame += p
+                previous_char = p
+
+        dots_to_be_added = len(game) - len(newGame)
+        for i in range(dots_to_be_added):
+            newGame += "."
+
+        compressed_games.append(newGame)
+    return compressed_games
+
+
+'''
+board = "BWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBWBW"
+repeatGame(board, 5000)
+boardSize = len(board)
+depth = 14
+count_of_positions_to_delete = boardSize - depth
+dotifyGames(boardSize, count_of_positions_to_delete)
+printGames()
+'''
+
+boardSize = 37
+generateGameCombinations(1000, boardSize)
+printGames()
+#print()
+#games_for_training = compress()
+printGames()
+
+
+def inference_black(board):
+    clobber = Clobber_1d(board, 1)  # Exp : White loses(0) [1 0]
+    clobber.computePrunedMovesFromSubgames(True)
+    X = clobber.board_features
+    X = np.reshape(X, (1, 40, 2))
+
+    start = time.time()
+    # Load TFLite model and allocate tensors.
+    interpreter = tf.lite.Interpreter(model_path="./clobber-black-cnn.tflite")
+    interpreter.allocate_tensors()
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], X)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    end = time.time()
+    d = end - start
+    # print("time taken ", d)
+    # print(output_data)
+    return output_data
+
+
+if TEST_BLACK:
+
+    count_correct = 0
+    count_incorrect = 0
+
+    print("Checking accuracy for Black")
+    for game in games_for_training:
+        #print(game)
+        clobber = Clobber_1d(game, 1)
+        play = PlayClobber()
+        test_start = time.time()
+        expected_outcome, _, _ = play.negamaxClobberGamePlay(clobber, test_start, 1000)
+        #print(expected_outcome)
+        predicted_outcome = inference_black(game)
+        #print(predicted_outcome)
+        if expected_outcome == PROVEN_WIN and predicted_outcome[0][1] > 0.7 and predicted_outcome[0][0] < 0.3:
+            count_correct += 1
+        elif expected_outcome == PROVEN_LOSS and predicted_outcome[0][1] < 0.3 and predicted_outcome[0][0] > 0.7:
+            count_correct += 1
+        else:
+            count_incorrect += 1
+
+    test_accuracy = 100 * (count_correct / (count_correct + count_incorrect))
+    print("Test Accuracy Black ", test_accuracy)
+
+
+# white
+
+
+def inference_white(board):
+    # print(board)
+    clobber = Clobber_1d(board, 1)  # Exp : White loses(0) [1 0]
+    clobber.computePrunedMovesFromSubgames(True)
+    X = clobber.board_features
+    X = np.reshape(X, (1, 40, 2))
+
+    start = time.time()
+    # Load TFLite model and allocate tensors.
+    interpreter = tf.lite.Interpreter(model_path="./clobber-white-cnn.tflite")
+    interpreter.allocate_tensors()
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], X)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    end = time.time()
+    d = end - start
+    # print("time taken ", d)
+    # print(output_data)
+    return output_data
+
+
+if TEST_WHITE:
+    # print(inference_white2("B.B.BBWW.W.BW.BW"))
+
+    count_correct = 0
+    count_incorrect = 0
+
+    print("Checking accuracy for White")
+    for game in games_for_training:
+        #print(game)
+        clobber = Clobber_1d(game, 2)
+        play = PlayClobber()
+        test_start = time.time()
+        expected_outcome, _, _ = play.negamaxClobberGamePlay(clobber, test_start, 1000)
+
+        predicted_outcome = inference_white(game)
+        if predicted_outcome is None:
+            continue
+        if expected_outcome == PROVEN_WIN and predicted_outcome[0][1] > 0.8 and predicted_outcome[0][0] < 0.2:
+            count_correct += 1
+        elif expected_outcome == PROVEN_LOSS and predicted_outcome[0][1] < 0.2 and predicted_outcome[0][0] > 0.8:
+            count_correct += 1
+        else:
+            count_incorrect += 1
+
+    test_accuracy = 100 * (count_correct / (count_correct + count_incorrect))
+    print("Test Accuracy White ", test_accuracy)
