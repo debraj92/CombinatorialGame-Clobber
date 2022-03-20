6c1716fc40c6a405ee8d4cf0e1166254bd105a10
Debraj Ray <debrajray@Debrajs-MacBook-Pro.local>
2022-03-20 16:18:44 -0600

    fix clobber test

--- clobber_test.py
+++ clobber_test.py
@@ -9,6 +9,42 @@ import numpy as np
 import tensorflow as tf
 
 
+def inference_black(board):
+    clobber = Clobber_1d(board, 1)  # Exp : White loses(0) [1 0]
+    clobber.computePrunedMovesFromSubgames(True)
+    X = clobber.board_features
+    X = np.reshape(X, (1, 40, 2))
+    # Load TFLite model and allocate tensors.
+    interpreter = tf.lite.Interpreter(model_path="./final-models/best3/clobber-black-cnn.tflite")
+    interpreter.allocate_tensors()
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], X)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data
+
+
+def inference_white(board):
+    clobber = Clobber_1d(board, 1)  # Exp : White loses(0) [1 0]
+    clobber.computePrunedMovesFromSubgames(True)
+    X = clobber.board_features
+    X = np.reshape(X, (1, 40, 2))
+    # Load TFLite model and allocate tensors.
+    interpreter = tf.lite.Interpreter(model_path="./final-models/best3/clobber-white-cnn.tflite")
+    interpreter.allocate_tensors()
+    # Get input and output tensors.
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], X)
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+    return output_data
+
+
 class clobberInstanceTests(unittest.TestCase):
     def test1(self):
         clobber = Clobber_1d("BWBW", BLACK)
@@ -28,7 +64,7 @@ class clobberInstanceTests(unittest.TestCase):
             for nextMove in move_set:
                 moves.append(nextMove)
 
-        self.assertTrue(moves[0] == (0, 1)) #black wins
+        self.assertTrue(moves[0] == (0, 1))  # black wins
         self.assertTrue((2, 1) in moves)
         self.assertTrue((2, 3) in moves)
         self.assertEqual(3, len(moves))
@@ -113,9 +149,9 @@ class clobberInstanceTests(unittest.TestCase):
         assert boardString == "W.BW...................................."
 
     def testBlackModel_1(self):
-        model_black = keras.models.load_model('clobber-black-cnn.h5')
+        model_black = keras.models.load_model('./final-models/best3/clobber-black-cnn.h5')
         clobber = Clobber_1d("BWB", BLACK, 1)  # Exp : Black wins(1) [0 1]
-                                                # we are evaluating from black's perspective.
+        # we are evaluating from black's perspective.
         clobber.computePrunedMovesFromSubgames(True)
         X = clobber.board_features
         X = np.reshape(X, (1, 40, 2))
@@ -125,9 +161,9 @@ class clobberInstanceTests(unittest.TestCase):
         assert prediction[0][0] < 0.3
 
     def testBlackModel_2(self):
-        model_black = keras.models.load_model('clobber-black-cnn.h5')
+        model_black = keras.models.load_model('./final-models/best3/clobber-black-cnn.h5')
         clobber = Clobber_1d("BBWW", BLACK, 1)  # Exp : Black loses(0) [1 0], ignore player -
-                                                # we are evaluating from black's perspective.
+        # we are evaluating from black's perspective.
         clobber.computePrunedMovesFromSubgames(True)
         X = clobber.board_features
         X = np.reshape(X, (1, 40, 2))
@@ -135,25 +171,43 @@ class clobberInstanceTests(unittest.TestCase):
         assert prediction[0][1] < 0.3
         assert prediction[0][0] > 0.7
 
-    def testWhiteModel_1(self):
-        model_black = keras.models.load_model('clobber-white-cnn.h5')
-        clobber = Clobber_1d("WB", BLACK, 1)  # Exp : White wins(1) [0 1]
-        clobber.computePrunedMovesFromSubgames(True)
-        X = clobber.board_features
-        X = np.reshape(X, (1, 40, 2))
-        prediction = model_black.predict(X)
+    def test_model_inference_1(self):
+
+        prediction = inference_white("WB")
+        assert prediction[0][1] < 0.3
+        assert prediction[0][0] > 0.7
+
+        prediction = inference_black("WB")
         assert prediction[0][1] > 0.7
         assert prediction[0][0] < 0.3
 
-    def testWhiteModel_2(self):
-        model_black = keras.models.load_model('clobber-white-cnn.h5')
-        clobber = Clobber_1d("BBWW", BLACK, 1)  # Exp : White loses(0) [1 0]
-        clobber.computePrunedMovesFromSubgames(True)
-        X = clobber.board_features
-        X = np.reshape(X, (1, 40, 2))
-        prediction = model_black.predict(X)
+    def test_model_inference_2(self):
+
+        prediction = inference_white("BBWW")
         assert prediction[0][1] < 0.3
         assert prediction[0][0] > 0.7
+
+        prediction = inference_black("BBWW")
+        assert prediction[0][1] < 0.3
+        assert prediction[0][0] > 0.7
+
+    def test_model_inference_3(self):
+
+        prediction = inference_white("BBW")
+        assert prediction[0][1] < 0.3
+        assert prediction[0][0] > 0.7
+
+        prediction = inference_black("BBW")
+        assert prediction[0][1] > 0.7
+        assert prediction[0][0] < 0.3
+
+        prediction = inference_white("BBW")
+        assert prediction[0][1] < 0.3
+        assert prediction[0][0] > 0.7
+
+        prediction = inference_black("BBW")
+        assert prediction[0][1] > 0.7
+        assert prediction[0][0] < 0.3
 
     def testchecktf(self):
         label = np.array([1, 0, 1, 1, 0])
@@ -162,7 +216,7 @@ class clobberInstanceTests(unittest.TestCase):
         print(label.shape)
 
     def testprediction(self):
-        model_black = keras.models.load_model('./clobber-black-cnn.h5')
+        model_black = keras.models.load_model('./final-models/best3/clobber-black-cnn.h5')
         clobber = Clobber_1d("BBW", BLACK, 1)  # Exp : White loses(0) [1 0]
         clobber.computePrunedMovesFromSubgames(True)
         X = clobber.board_features
