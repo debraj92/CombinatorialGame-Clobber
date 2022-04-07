f369731aa34ee44a6a08e78d0f29dff607cff479
Akash Saravanan <akashsara@outlook.com>
2022-04-07 15:09:26 -0600

    allow move ordering selection in tests

--- ml_accuracy_tests.py
+++ ml_accuracy_tests.py
@@ -12,19 +12,31 @@ PROVEN = 0
 DISPROVEN = 1
 INFINITY = 1000000
 
-
-play = PlayClobber()
 totalPlayer = BLACK + WHITE
 
 
 class clobberCNN_AccuracyTests(unittest.TestCase):
 
     def test1(self):
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": False,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         game = "BWBWBWBWBWBW"
         first_player = BLACK
         clobber = Clobber_1d(game, first_player)
-        clobber.cnn_enabled = True
+        clobber.cnn_enabled = True if move_ordering["cnn"] else False
         outcome1, winning_move, nodes = play.negamaxClobberGamePlay(clobber, time.time())
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": True,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         if outcome1 == PROVEN_WIN:
             clobber2 = Clobber_1d(game, first_player)
             clobber2.cnn_enabled = False
@@ -38,11 +50,25 @@ class clobberCNN_AccuracyTests(unittest.TestCase):
         self.assertTrue(outcome2 == PROVEN_LOSS)
 
     def test2(self):
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": False,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         game = "WBWBWBWBWBWBWBWBW"
         first_player = WHITE
         clobber = Clobber_1d(game, first_player)
-        clobber.cnn_enabled = True
+        clobber.cnn_enabled = True if move_ordering["cnn"] else False
         outcome1, winning_move, nodes = play.negamaxClobberGamePlay(clobber, time.time())
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": True,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         if outcome1 == PROVEN_WIN:
             clobber2 = Clobber_1d(game, first_player)
             clobber2.cnn_enabled = False
@@ -56,11 +82,25 @@ class clobberCNN_AccuracyTests(unittest.TestCase):
         self.assertTrue(outcome2 == PROVEN_LOSS)
 
     def test3(self):
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": False,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         game = "WBWBWBBWBWBWBWBBBWBWWB..WBWBW"
         first_player = WHITE
         clobber = Clobber_1d(game, first_player)
-        clobber.cnn_enabled = True
+        clobber.cnn_enabled = True if move_ordering["cnn"] else False
         outcome1, winning_move, nodes = play.negamaxClobberGamePlay(clobber, time.time())
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": True,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         if outcome1 == PROVEN_WIN:
             clobber2 = Clobber_1d(game, first_player)
             clobber2.cnn_enabled = False
@@ -74,11 +114,25 @@ class clobberCNN_AccuracyTests(unittest.TestCase):
         self.assertTrue(outcome2 == PROVEN_LOSS)
 
     def test4(self):
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": False,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         game = "BBW..BBW"
         first_player = WHITE
         clobber = Clobber_1d(game, first_player)
-        clobber.cnn_enabled = True
+        clobber.cnn_enabled = True if move_ordering["cnn"] else False
         outcome1, winning_move, nodes = play.negamaxClobberGamePlay(clobber, time.time())
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": True,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         if outcome1 == PROVEN_WIN:
             clobber2 = Clobber_1d(game, first_player)
             clobber2.cnn_enabled = False
@@ -92,11 +146,25 @@ class clobberCNN_AccuracyTests(unittest.TestCase):
         self.assertTrue(outcome2 == PROVEN_LOSS)
 
     def test5(self):
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": False,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         game = "BB..B"
         first_player = WHITE
         clobber = Clobber_1d(game, first_player)
-        clobber.cnn_enabled = True
+        clobber.cnn_enabled = True if move_ordering["cnn"] else False
         outcome1, winning_move, nodes = play.negamaxClobberGamePlay(clobber, time.time())
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": True,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         if outcome1 == PROVEN_WIN:
             clobber2 = Clobber_1d(game, first_player)
             clobber2.cnn_enabled = False
@@ -110,11 +178,25 @@ class clobberCNN_AccuracyTests(unittest.TestCase):
         self.assertTrue(outcome2 == PROVEN_LOSS)
 
     def test6(self):
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": False,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         game = "BBWBBWWWBWWBWWBBBW"
         first_player = BLACK
         clobber = Clobber_1d(game, first_player)
-        clobber.cnn_enabled = True
+        clobber.cnn_enabled = True if move_ordering["cnn"] else False
         outcome1, winning_move, nodes = play.negamaxClobberGamePlay(clobber, time.time())
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": True,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         if outcome1 == PROVEN_WIN:
             clobber2 = Clobber_1d(game, first_player)
             clobber2.cnn_enabled = False
@@ -128,11 +210,25 @@ class clobberCNN_AccuracyTests(unittest.TestCase):
         self.assertTrue(outcome2 == PROVEN_LOSS)
 
     def test7(self):
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": False,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         game = "WBWBWBWBWBWBWBWBWBWBWBWBWBWB"
         first_player = BLACK
         clobber = Clobber_1d(game, first_player)
-        clobber.cnn_enabled = True
+        clobber.cnn_enabled = True if move_ordering["cnn"] else False
         outcome1, winning_move, nodes = play.negamaxClobberGamePlay(clobber, time.time())
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": True,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         if outcome1 == PROVEN_WIN:
             clobber2 = Clobber_1d(game, first_player)
             clobber2.cnn_enabled = False
@@ -146,11 +242,25 @@ class clobberCNN_AccuracyTests(unittest.TestCase):
         self.assertTrue(outcome2 == PROVEN_LOSS)
 
     def test8(self):
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": False,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         game = "WBWBWBWBWBWBWBWBWBWBWBWBWBWB"
         first_player = WHITE
         clobber = Clobber_1d(game, first_player)
-        clobber.cnn_enabled = True
+        clobber.cnn_enabled = True if move_ordering["cnn"] else False
         outcome1, winning_move, nodes = play.negamaxClobberGamePlay(clobber, time.time())
+        move_ordering = {
+            "rl": False,
+            "cnn": True,
+            "default": True,
+            "none": False
+        }
+        play = PlayClobber(move_ordering)
         if outcome1 == PROVEN_WIN:
             clobber2 = Clobber_1d(game, first_player)
             clobber2.cnn_enabled = False
